6d9822d7d1efe6dca3c5dbb5471a176a8b5ae1a5
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2018-11-27 21:32:43 +0000

    Added reat-testing-library and test for App componnet

--- src/__tests__/app_tests.js
+++ src/__tests__/app_tests.js
@@ -1,5 +1,6 @@
 import App from "../components/App/App";
-
+import { render } from "react-testing-library";
 test("App should render correctly", () => {
-  expect(<App />).toMatchSnapshot();
+  const { container } = render(<App />);
+  expect(container).toMatchSnapshot();
 });
